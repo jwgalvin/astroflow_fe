45990444727cf5e04149b650491a8871fcdce094
Joseph W Galvin <jwgalvin@gmail.com>
2022-04-15 13:44:21 -0600

    Log in page done pending Oauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
-
+  helper_method :current_user
+  
   def error_message(errors)
     errors.full_messages.join(', ')
+  end
+
+  def current_user
+    session[:access_token].present? #&& UserFacade.get_by_email(session[:email])
+  end
+
+  def user
+    user.find(session[:user_id]) if session[:user_id]
   end
 end
