f97180cf9fd2a9f7a405b5ed2eb7570cbf58ffa0
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-17 19:49:21 -0400

    Fix: Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
 
-
     root "welcome#index"
     get '/auth/google_oauth2/callback', to: 'users#create'
     get '/logout', to: 'users#logout'
-    get '/register', to: 'users#edit'
+    get '/edit', to: 'users#edit'
     patch '/register', to: 'users#update'
+    get '/flowchart', to: 'flowchart#index'
     resources :login, only: [:index]
-    resources :dashboard, only: [:index], controller: :users do
-      resources :flowchart, only: [:show], controller: :flowchart
-    end
+    resources :dashboard, only: [:index], controller: :users 
     resources :logout, only: [:logout], controller: :users
-    end
+end
