e84c143854a24ec85282cc530cf787c5aa3382f2
Sully Birashk <sully.birashk@gmail.com>
2022-04-20 16:29:07 -0700

    Flowchart Service Test

---
+++ spec/services/flowchart_service_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe FlowchartService do
+  it "returns flow chart" do
+    email = 'test@gmail.com'
+    flow_chart = {bloating: true, cramps: false, emotions: "N/A", flow_status: :heavy}
+    #search = FlowchartService.call_to_send_flowchart(email, flow_chart)
+    # expect(search).to be_a Hash
+    #once we get flowchart working, this test will take care of flow chart services
+  end
+end
