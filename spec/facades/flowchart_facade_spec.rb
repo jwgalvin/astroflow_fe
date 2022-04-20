0d0fa304538a75ab441fa0b3d68f31cf4f8b0dd4
Sully Birashk <sully.birashk@gmail.com>
2022-04-20 16:28:36 -0700

    Flowchart Facade Test

---
+++ spec/facades/flowchart_facade_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe FlowchartFacade do
+  it "will return flow chart" do
+    email = 'test@testing.com'
+    # flow_chart = FlowchartFacade.send_flowchart
+  end
+end
