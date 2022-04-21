2b5c9e0aa05841e833bb4dad6133012066e0497a
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-21 15:44:18 -0400

    Add: Flowchart facade & services

--- app/facades/flowchart_facade.rb
+++ app/facades/flowchart_facade.rb
@@ -1,7 +1,23 @@
 class FlowchartFacade 
   class << self 
     def send_flowchart(email, flow_chart)
-      FlowchartService.call_to_send_flowchart(email, flow_chart)
+      FlowchartService.call_to_create_flowchart(email, flow_chart)
+    end 
+
+    def update_flowchart(email, date, flow_chart)
+      FlowchartService.call_to_update_flowchart(email, date,flow_chart)
+    end 
+
+    
+
+    def flow_chart(flow_chart_data)
+      DailyFlowChart.new(
+            date: flow_chart_data["date"],
+            bloating: flow_chart_data["bloating"],   
+            cramps: flow_chart_data["cramps"],     
+            emotions: flow_chart_data["emotions"],
+            flow_status: flow_chart_data["flow_status"]
+      )
     end 
   end 
 end 
--- app/services/flowchart_service.rb
+++ app/services/flowchart_service.rb
@@ -1,13 +1,17 @@
 class FlowchartService  
   class << self 
-    def call_to_send_flowchart(email, flow_chart)
-      response = conn.post("/api/v1/users/daily_flow_chart?email=#{@user_email}&daily_flow_chart=#{flow_chart}")
+    def call_to_create_flowchart(email, flow_chart)
+      response = conn.post("/api/v1/users/daily_flow_chart?email=#{email}&daily_flow_chart=#{flow_chart}")
+    end 
+
+    def call_to_update_flowchart(email, date, flow_chart)
+      response = conn.patch("/api/v1/users/daily_flow_chart?email=#{email}&date=#{date}&daily_flow_chart=#{flow_chart}")
     end 
 
     private 
 
     def conn 
-      Faraday.new(url: "https://astro-flow-be.herokuapp.com")
+      Faraday.new(url: "http://localhost:5000")
     end 
   end 
 end 
